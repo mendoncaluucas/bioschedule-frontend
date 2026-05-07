ea70072dc5d7e18df0524c3c63efa84c08e7c87b
mendoncaluucas <mendoncalucas348@gmail.com>
2026-05-07 15:51:41 -0300

    fix: arrumando importacao do analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { Analytics } from "@vercel/analytics/next"
 import './index.css'
 import App from './App.tsx'
+
+import { Analytics } from "@vercel/analytics/react"
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
